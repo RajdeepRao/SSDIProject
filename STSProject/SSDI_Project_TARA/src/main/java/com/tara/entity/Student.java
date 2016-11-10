6964188ff2d52ccaf14ab7b10e63e08d51921708
Sachin Badgujar <bsachin207@gmail.com>
2016-11-10 18:56:00 -0500

    Before Pull

--- STSProject/SSDI_Project_TARA/src/main/java/com/tara/entity/Student.java
+++ STSProject/SSDI_Project_TARA/src/main/java/com/tara/entity/Student.java
@@ -78,4 +78,59 @@ public class Student {
 	public void setNinerId(int ninerId) {
 		this.ninerId = ninerId;
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((emailId == null) ? 0 : emailId.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ninerId;
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (emailId == null) {
+			if (other.emailId != null)
+				return false;
+		} else if (!emailId.equals(other.emailId))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (ninerId != other.ninerId)
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		return true;
+	}
 }
---
+++ STSProject/SSDI_Project_TARA/src/test/java/com/tara/StudentServiceTest.java
@@ -0,0 +1,58 @@
+package com.tara;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.tara.dao.StudentDAO;
+import com.tara.entity.Student;
+import com.tara.service.StudentService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentServiceTest {
+	
+	@Autowired
+	private StudentService studentservice;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+	}
+
+	@Test
+	public void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateStudent() {
+		int id = 7;
+		String firstName="Rumit";
+		String lastName="Gajera";
+		String emailId="sgajera@gmail.com";
+		String password="xyz";
+		int ninerId=800902445;
+		Student student1 = new Student(id, firstName, lastName, emailId, password, ninerId);
+		studentservice.createStudent(student1);
+		Student student2 = studentservice.findById(7);
+		assert(student1.equals(student2));
+
+	}
+
+	@Test
+	public void testUpdateStudent() {
+		fail("Not yet implemented");
+	}
+
+}
